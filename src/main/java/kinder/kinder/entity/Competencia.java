ee614f92627d1a8fb2df08dbf401e2f1e564856f
Hzsxz <jimmyeog14@gmail.com>
2024-06-06 20:18:44 -0500

    xd

--- src/main/java/kinder/kinder/entity/Competencia.java
+++ src/main/java/kinder/kinder/entity/Competencia.java
@@ -1,25 +1,20 @@
 package kinder.kinder.entity;
-import java.util.Collection;
-import java.util.List;
-
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.persistence.UniqueConstraint;
 
 @Entity
-@Table(name="competencia", uniqueConstraints = {@UniqueConstraint(columnNames = {"username"})})
+@Table(name="competencia")
 
 public class Competencia {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	Integer id;
-	@Column
+	@Column(nullable = false)
 	String competencia;
 	String descripcion;
 	public Integer getId() {
--- src/main/java/kinder/kinder/entity/Curso.java
+++ src/main/java/kinder/kinder/entity/Curso.java
@@ -1,25 +1,20 @@
 package kinder.kinder.entity;
-import java.util.Collection;
-import java.util.List;
-
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.persistence.UniqueConstraint;
 
 @Entity
-@Table(name="competencia", uniqueConstraints = {@UniqueConstraint(columnNames = {"username"})})
+@Table(name="curso")
 
 public class Curso {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	Integer id;
-	@Column
+	@Column(nullable = false)
 	String curso;
 	String descripcion;
 	
