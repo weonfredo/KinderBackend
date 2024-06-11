844ef19eb64ec9aaf0eaa305b0f0151db693135d
EduGalaxy0 <eduysting@hotmail.com>
2024-06-11 14:13:15 -0500

    backend con controller notas y asistencia agregado

---
+++ src/main/java/kinder/kinder/controller/AsistenciaController.java
@@ -0,0 +1,47 @@
+package kinder.kinder.controller;
+import kinder.kinder.entity.Alumno;
+import kinder.kinder.entity.Asistencia;
+import kinder.kinder.service.jpa.AlumnoService;
+import kinder.kinder.service.jpa.AsistenciaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Date;
+import java.util.List;
+
+@Controller
+public class AsistenciaController {
+
+    @Autowired
+    private AlumnoService alumnoService;
+
+    @Autowired
+    private AsistenciaService asistenciaService;
+
+    @GetMapping("/asistencias")
+    public String getAsistencias(Model model) {
+        List<Alumno> alumnos = alumnoService.buscarTodos();
+        model.addAttribute("alumnos", alumnos);
+        return "asistencias";
+    }
+
+    @PostMapping("/asistencias")
+    public String saveAsistencias(Model model, String[] alumnoIds, String[] estados) {
+        for (int i = 0; i < alumnoIds.length; i++) {
+            Integer alumnoId = Integer.parseInt(alumnoIds[i]);
+            Alumno alumno = new Alumno();
+            alumno.setId(alumnoId);
+            
+            Asistencia asistencia = new Asistencia();
+            asistencia.setAlumno(alumno);
+            asistencia.setFecha(new Date());
+            asistencia.setEstado(estados[i]);
+
+            asistenciaService.save(asistencia);
+        }
+        return "redirect:/asistencias";
+    }
+}
---
+++ src/main/java/kinder/kinder/controller/NotasController.java
@@ -0,0 +1,77 @@
+package kinder.kinder.controller;
+
+import kinder.kinder.entity.Alumno;
+import kinder.kinder.entity.Competencia;
+import kinder.kinder.entity.Curso;
+import kinder.kinder.entity.Nota;
+import kinder.kinder.repository.AlumnoRepository;
+import kinder.kinder.service.jpa.AlumnoService;
+import kinder.kinder.service.jpa.CompetenciaService;
+import kinder.kinder.service.jpa.CursoService;
+import kinder.kinder.service.jpa.NotaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+public class NotasController {
+
+    @Autowired
+    private AlumnoService alumnoService;
+
+    @Autowired
+    private NotaService notaService;
+
+    @Autowired
+    private CompetenciaService competenciaService;
+
+    @Autowired
+    private CursoService cursoService;
+    
+    @Autowired
+    private AlumnoRepository alumnoRepository;
+
+    @GetMapping("/notas")
+    public String getNotas(Model model) {
+        List<Alumno> alumnos = alumnoService.buscarTodos();
+        List<Competencia> competencias = competenciaService.buscarTodos();
+        List<Curso> cursos = cursoService.buscarTodos();
+        model.addAttribute("alumnos", alumnos);
+        model.addAttribute("competencias", competencias);
+        model.addAttribute("cursos", cursos);
+        return "notas";
+    }
+
+    @PostMapping("/notas")
+    public String saveNotas(@RequestParam("alumnoIds") String[] alumnoIds,
+				            @RequestParam("notas") Double[] notas,
+				            @RequestParam("competenciaIds") Integer[] competenciaIds,
+				            @RequestParam("cursoIds") Integer[] cursoIds) {
+			for (int i = 0; i < alumnoIds.length; i++) {
+			Integer alumnoId = Integer.parseInt(alumnoIds[i]);
+			Optional<Alumno> optionalAlumno = alumnoRepository.findById(alumnoId);
+			if (optionalAlumno.isPresent()) {
+			Alumno alumno = optionalAlumno.get();
+			
+			// Guardar nota
+			Nota nota = new Nota();
+			nota.setAlumno(alumno);
+			nota.setValor(notas[i]);
+			Competencia competencia = new Competencia();
+			competencia.setId(competenciaIds[i]);
+			nota.setCompetencia(competencia);
+			Curso curso = new Curso();
+			curso.setId(cursoIds[i]);
+			nota.setCurso(curso);
+			notaService.save(nota);
+			}
+			}
+			return "redirect:/notas";
+			}
+}
