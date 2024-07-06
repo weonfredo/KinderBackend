aa9293f870cd5c6b069afb840f838161797f2d78
Hzsxz <jimmyeog14@gmail.com>
2024-07-06 11:34:47 -0500

    a

--- src/main/java/kinder/kinder/controller/NotasController.java
+++ src/main/java/kinder/kinder/controller/NotasController.java
@@ -14,12 +14,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Optional;
 
-@Controller
+@RestController
+@RequestMapping("/nota")
 public class NotasController {
 
     @Autowired
